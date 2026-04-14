64137f9a087eaf1141efeca1d915aa9f509acd53
hayoungshin1 <64515255+hayoungshin1@users.noreply.github.com>
2026-04-15 00:43:34 +0900

    Update hyperbolicexperiments.py

--- hyperbolicexperiments.py
+++ hyperbolicexperiments.py
@@ -10,7 +10,7 @@ origin[:,:,0,:]=1 #(1,1,n+1,1)
 vs=np.identity(2)
 vs=np.expand_dims(vs,(0,1)) #(1,1,n,2)
 
-N=2000000
+N=20000000
 covariates=np.random.uniform(size=(1,N,2,1))-0.5 #(1,N,2,1)
 S=2-(np.expand_dims(covariates[:,:,0,:],2)>=0) #(1,N,1,1)
 Z=np.zeros((1,N,1,1))
@@ -31,42 +31,14 @@ wT=0.5*Z*(S==1)/np.sum(Z*(S==1),1,keepdims=True)+0.5*Z*(S==2)/np.sum(Z*(S==2),1,
 trueans=invbroyden(R,wC,fmean(R,wT,tol),tol)
 print(trueans)
 
-# classical randomized experiment-consistency (don't think this is needed in light of the strong consistency theorem)
-
-np.random.seed(1)
-
-reps=1000
-Ns=[125,250,500,1000]
-expconsistency=[0,0,0,0]
-for j in range(4):
-    N=Ns[j]
-    covariates=np.random.uniform(size=(reps,N,2,1))-0.5 #(reps,N,2,1)
-    S=2-(np.expand_dims(covariates[:,:,0,:],2)>=0) #(reps,N,1,1)
-    Z=np.zeros((reps,N,1,1))
-    for s in [1,2]:
-        for rep in range(reps):
-            mN=np.sum(S[rep,:,:,:]==s)
-            indices=np.where(S[rep,:,:,:]==s)[0]
-            Z[rep,np.random.choice(indices,int((mN+1)/2),replace=False),:,:]=1
-    temp=np.zeros([1,1,n,1])
-    temp[:,:,0,:]=1 #(1,1,n,1)
-    tans=Z*temp-(1-Z)*temp+np.random.normal(size=(reps,N,2,1))+np.matmul(vs,covariates) #(reps,N,n,1)
-    zs=np.zeros((reps,N,1,1))
-    tans=np.concatenate((zs,tans),axis=2)
-    origins=np.tile(origin,(reps,N,1,1))
-    R=exp(origins,tans,tol) #(reps,N,n+1,1)
-    wC=0.5*(1-Z)*(S==1)/np.sum((1-Z)*(S==1),1,keepdims=True)+0.5*(1-Z)*(S==2)/np.sum((1-Z)*(S==2),1,keepdims=True) #(1,N,1,1)
-    wT=0.5*Z*(S==1)/np.sum(Z*(S==1),1,keepdims=True)+0.5*Z*(S==2)/np.sum(Z*(S==2),1,keepdims=True) #(1,N,1,1)
-    ans=invbroyden(R,wC,fmean(R,wT,tol),tol)
-    expconsistency[j]=np.mean(np.sqrt(np.sum((trueans-ans)**2,2)),0).item()
-    print(expconsistency[j])
-
 # classical randomized experiment-confidence region
 
-np.random.seed(1)
+np.random.seed(10)
 
+B=1000
 Ns=[125,250,500,1000]
-expcounter=[0,0,0,0]
+expcounter1=[0,0,0,0]
+expcounter2=[0,0,0,0]
 for j in range(4):
     N=Ns[j]
     for k in range(1000):
@@ -86,8 +58,7 @@ for j in range(4):
         R=exp(origins,tans,tol) #(1,N,n+1,1)
         wC=0.5*(1-Z)*(S==1)/np.sum((1-Z)*(S==1),1,keepdims=True)+0.5*(1-Z)*(S==2)/np.sum((1-Z)*(S==2),1,keepdims=True) #(1,N,1,1)
         wT=0.5*Z*(S==1)/np.sum(Z*(S==1),1,keepdims=True)+0.5*Z*(S==2)/np.sum(Z*(S==2),1,keepdims=True) #(1,N,1,1)
-        ans=invbroyden(R,wC,fmean(R,wT,tol),tol)
-        B=1000
+        ans=np.squeeze(invbroyden(R,wC,fmean(R,wT,tol),tol),axis=1)[:,1:3,:]
         bootR=np.empty((B,N,n+1,1))
         bootS=np.empty((B,N,1,1))
         bootZ=np.empty((B,N,1,1))
@@ -98,12 +69,19 @@ for j in range(4):
             bootZ[b,:,:,:]=Z[0,bootindices,:,:]
         bootwC=0.5*(1-bootZ)*(bootS==1)/np.sum((1-bootZ)*(bootS==1),1,keepdims=True)+0.5*(1-bootZ)*(bootS==2)/np.sum((1-bootZ)*(bootS==2),1,keepdims=True) #(B,N,1,1)
         bootwT=0.5*bootZ*(bootS==1)/np.sum(bootZ*(bootS==1),1,keepdims=True)+0.5*bootZ*(bootS==2)/np.sum(bootZ*(bootS==2),1,keepdims=True) #(B,N,1,1)
-        bootans=invbroyden(bootR,bootwC,fmean(bootR,bootwT,tol),tol)
-        if np.squeeze(np.sqrt(np.sum((trueans-ans)**2)))<=np.sort(np.squeeze(np.sqrt(np.sum((bootans-ans)**2,2))))[int(0.95*B)]:
-            expcounter[j]+=1
+        bootans=np.squeeze(invbroyden(bootR,bootwC,fmean(bootR,bootwT,tol),tol),axis=1)[:,1:3,:]
+        if np.squeeze(np.sqrt(np.sum((np.squeeze(trueans,axis=1)[:,1:3,:]-ans)**2)))<=np.sort(np.squeeze(np.sqrt(np.sum((bootans-ans)**2,1))))[int(0.95*B)]:
+            expcounter1[j]+=1
+        covinv=np.linalg.inv(np.expand_dims(np.cov(np.squeeze(bootans), rowvar=False), axis=0))
+        bootstats=np.zeros(B)
+        for b in range(B):
+            bootstats[b]=np.squeeze(np.matmul(np.matmul(np.transpose(bootans[b:(b+1),:,:]-ans,(0,2,1)),covinv),bootans[b:(b+1),:,:]-ans))
+        teststatistic=np.squeeze(np.matmul(np.matmul(np.transpose(ans-np.squeeze(trueans,axis=1)[:,1:3,:],(0,2,1)),covinv),ans-np.squeeze(trueans,axis=1)[:,1:3,:]))
+        if teststatistic<=np.sort(bootstats)[int(0.95*B)]:
+            expcounter2[j]+=1
         if k % 10==0:
-            print(N, k, expcounter[j])
-    print(expcounter[j])
+            print(N, k, expcounter1[j], expcounter2[j])
+    print(expcounter1[j], expcounter2[j])
 
 # matched observational study-consistency
 
@@ -153,12 +131,14 @@ for j in range(4):
 
 # matched observational study-confidence region
 
+B=1000
 Ns=[125,250,500,1000]
-OSexpcounter=[0,0,0,0]
+OSexpcounter1=[0,0,0,0]
+OSexpcounter2=[0,0,0,0]
 for j in range(4):
     N=Ns[j]
     for k in range(1000):
-        np.random.seed(1000*j+k)
+        np.random.seed(10000*j+k)
         covariates=np.random.uniform(size=(1,N,2,1))-0.5 #(1,N,2,1)
         Z=np.zeros((1,N,1,1))
         S=np.zeros((1,N,1,1))
@@ -191,8 +171,7 @@ for j in range(4):
         tans=np.concatenate((zs,tans),axis=2)
         origins=np.tile(origin,(1,N,1,1))
         R=exp(origins,tans,tol) #(1,N,n+1,1)
-        ans=invbroyden(R,wC,fmean(R,wT,tol),tol)
-        B=1000
+        ans=np.squeeze(invbroyden(R,wC,fmean(R,wT,tol),tol),axis=1)[:,1:3,:]
         bootR=np.empty((B,N,n+1,1))
         bootS=np.empty((B,N,1,1))
         bootZ=np.empty((B,N,1,1))
@@ -219,13 +198,18 @@ for j in range(4):
                 wTrep[s-1,:,0,0]=lambdas*(bootZ[b,:,0,0])*(bootS[b,:,0,0]==s)/np.sum((bootZ[b,:,0,0])*(bootS[b,:,0,0]==s))
             bootwC[b,:,0,0]=np.sum(wCrep,axis=0)[:,0,0]
             bootwT[b,:,0,0]=np.sum(wTrep,axis=0)[:,0,0]
-        bootans=invbroyden(bootR,bootwC,fmean(bootR,bootwT,tol),tol)
-        if np.squeeze(np.sqrt(np.sum((trueans-ans)**2)))<=np.sort(np.squeeze(np.sqrt(np.sum((bootans-ans)**2,2))))[int(0.95*B)]:
-            OSexpcounter[j]+=1
+        bootans=np.squeeze(invbroyden(bootR,bootwC,fmean(bootR,bootwT,tol),tol),axis=1)[:,1:3,:]
+        if np.squeeze(np.sqrt(np.sum((np.squeeze(trueans,axis=1)[:,1:3,:]-ans)**2)))<=np.sort(np.squeeze(np.sqrt(np.sum((bootans-ans)**2,1))))[int(0.95*B)]:
+            OSexpcounter1[j]+=1
+        bootstats=np.zeros(B)
+        covinv=np.linalg.inv(np.expand_dims(np.cov(np.squeeze(bootans), rowvar=False), axis=0))
+        for b in range(B):
+            bootstats[b]=np.squeeze(np.matmul(np.matmul(np.transpose(bootans[b:(b+1),:,:]-ans,(0,2,1)),covinv),bootans[b:(b+1),:,:]-ans))
+        teststatistic=np.squeeze(np.matmul(np.matmul(np.transpose(ans-np.squeeze(trueans,axis=1)[:,1:3,:],(0,2,1)),covinv),ans-np.squeeze(trueans,axis=1)[:,1:3,:]))
+        if teststatistic<=np.sort(bootstats)[int(0.95*B)]:
+            OSexpcounter2[j]+=1
         if k % 10==0:
-            print(N, k, OSexpcounter[j])
-    print(OSexpcounter[j])
+            print(N, k, OSexpcounter1[j], OSexpcounter2[j])
+    print(OSexpcounter1[j], OSexpcounter2[j])
 
-print(OSexpcounter)
-
-#np.save('Documents/OSexpcounter.npy',OSexpcounter)
+print(OSexpcounter1, OSexpcounter2)
